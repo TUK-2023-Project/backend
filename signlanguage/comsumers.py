638e02650368e1e3e3c1098065bf4816c5f9afd5
teawon <xodnjsvkvk9@gmail.com>
2023-03-05 16:17:49 +0900

    feat : 웹소켓 AI 실행 함수 연결

--- signlanguage/comsumers.py
+++ signlanguage/comsumers.py
@@ -1,18 +1,15 @@
 import asyncio
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
+from AI import predict
 
 class SignLanguageConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         await self.accept()
-
-        # Initialize a variable to keep track of the total size of received video data
-        self.total_size = 0
+  
 
       
-
     async def receive(self, text_data):
-        # Get the video data from the front-end
         video_data = json.loads(text_data)
 
   
@@ -23,15 +20,8 @@ class SignLanguageConsumer(AsyncWebsocketConsumer):
                  if isinstance(sub_item, dict):
                     landmarks.append({"landmarks": sub_item['landmarks']})
 
-        print(landmarks)
-        
-        
-        # landmarks 데이터 AI처리
-        # AI처리된 결과를 return하기
-
-
 
         await self.send(text_data=json.dumps({
-            'message': 'Data received successfully'
+            'message': predict.run(landmarks)
         }))
 
