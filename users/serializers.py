d25c2c0b25e3aaa1b665df282c4e1db3dc792461
수연 <syeon0131@naver.com>
2023-04-15 17:04:20 +0900

    fix:로그인 예외처리

--- users/serializers.py
+++ users/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 from .models import UserData
-
-
+import json
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from django.http import JsonResponse
 class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
@@ -15,3 +16,33 @@ class UserSerializer(serializers.ModelSerializer):
         user.set_password(validated_data['password'])
         user.save()
         return user
+
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+    
+        od1 = json.dumps(attrs)
+        od2 = json.loads(od1)
+
+        
+        name=UserData.objects.filter(email = od2['email']).first()
+
+        if name:
+            if not name.check_password(od2['password']) :
+
+                data1={"message":"비밀번호 없습니다.","stats":400}
+                return data1
+
+            else:
+                data1="success"
+      
+        else:
+            
+            data1= {"message":"이메일 없습니다.","stats":400}
+            return data1
+
+        data = super(CustomTokenObtainPairSerializer, self).validate(attrs)
+       
+        return data
--- users/views.py
+++ users/views.py
@@ -49,3 +49,15 @@ def NameCheck(request):
 
         else:
             return JsonResponse({"status":200})
+
+
+from .serializers import CustomTokenObtainPairSerializer
+
+from rest_framework_simplejwt.views import TokenObtainPairView
+class CustomTokenObtainPairView(TokenObtainPairView):
+    # Replace the serializer with your custom
+
+   
+    serializer_class = CustomTokenObtainPairSerializer
+   
+
