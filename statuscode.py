f52e047c8ebb06adf44f161d1a25d6ebc5b8279d
수연 <syeon0131@naver.com>
2023-04-16 16:45:48 +0900

    fix:statuscode

--- statuscode.py
+++ statuscode.py
@@ -1,4 +1,4 @@
-TOKEN_ERROR={'message': '토큰이 필요합니다',"code":"100"}
+TOKEN_ERROR={'message': '토큰이 필요합니다',"CODE":"100"}
 SUCCESS={"status":200}
 NO_USER={'message': '존재하지 않는 유저 입니다',"CODE":"101"}
 NO_USER_INFO={'message': '해당 유저의 랭킹 정보가 없습니다.',"CODE":"102"}
