045268092c7a91cf00580f4b7d4900c85abf09e2
수연 <syeon0131@naver.com>
2023-05-06 20:47:02 +0900

    feat:리스트로 출력

--- incorrect_note/urls.py
+++ incorrect_note/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     #path('upload/', ImageUploader.as_view(), name="upload"),
     path('add/', views.add_note, name="add"),
     path('delete/', views.delete_note, name="delete"),
+    path('list/', views.show_list_note, name="list"),
+    
 
 
     
--- incorrect_note/views.py
+++ incorrect_note/views.py
@@ -6,6 +6,7 @@ from rest_framework import status
 from django.http import JsonResponse
 import jwt
 from utils import *
+from .serializers import *
 # Create your views here.
 from .models import *
 from statuscode import *
@@ -36,10 +37,7 @@ def add_note(request):
 
         return Response(SUCCESS,status=status.HTTP_200_OK)
 
-        #추가하는 값이 is deleted가 false인데 다시 값 있으면 
-        #Item.objects.filter(create_dt__year=2020).update(available=False)
-        #만약에 db에 지워졌는데 ㅣㅇㅆ으면 유저랑 사인 아이디의 값이 이미 존재하는 필드라면 is deleted를 바꾸고 
-        #아니라면 else로 위 코드 실행 한다.
+        
 
 @api_view(['GET'])
 def delete_note(request):
@@ -61,3 +59,38 @@ def delete_note(request):
         sign_id=SignWord.objects.get(sign_id=sign_id)
         Incorret.objects.filter(user_id=id, sign_id=sign_id).update(is_deleted=True)
         return Response(SUCCESS,status=status.HTTP_200_OK)
+
+
+@api_view(['GET'])
+def show_list_note(request):
+        token = request.META.get('HTTP_ACCESS', None)
+        if token is None:
+            return Response(TOKEN_ERROR,status=status.HTTP_404_NOT_FOUND)
+
+        try:
+            user_id = get_user_id_from_token(token.split(' ')[1])
+
+        except jwt.exceptions.ExpiredSignatureError:
+            return Response(TOKEN_EXPIRE, status=status.HTTP_401_UNAUTHORIZED)
+
+        except jwt.exceptions.DecodeError:
+            return Response(TOKEN_VAILD, status=status.HTTP_401_UNAUTHORIZED)
+    
+        response_data = []
+
+        id=UserData.objects.get(id=user_id)
+        sign_list = Incorret.objects.filter(user_id=id).values("sign_id")
+
+        for i in sign_list:
+            string=str(i["sign_id"])
+            response_data.append(string)
+
+        response_data2 = []
+
+        for i in response_data:
+            count=0
+            a=SignWord.objects.get(sign_id=i[count])
+            count=count+1
+            response_data2.append(Listserializer(a).data)
+        
+        return JsonResponse({'data_list':response_data2})
